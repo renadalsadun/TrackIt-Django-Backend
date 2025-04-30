663e2da7d10a080d17881d6b674632ad49340d09
renad <xirnzsixteen@hotmail.com>
2025-05-01 01:13:20 +0300

    created Application model

--- trackit_project_backend/trackit/models.py
+++ trackit_project_backend/trackit/models.py
@@ -10,3 +10,29 @@ class Tracker(models.Model):
 
     def __str__(self):
         return self.name
+    
+
+
+class Application(models.Model): 
+    name = models.CharField(max_length=100)
+    tracker = models.ForeignKey(Tracker, on_delete=models.CASCADE)
+
+    company = models.CharField(max_length=100, blank=True )
+    organization = models.CharField(max_length=100, blank=True )
+    university = models.CharField(max_length=100, blank=True )
+    location = models.CharField(max_length=100, blank=True )
+    website = models.CharField(max_length=100, blank=True )
+    link = models.CharField(max_length=100, blank=True )
+    description = models.CharField(max_length=100, blank=True )
+    referral = models.CharField(max_length=100, blank=True )
+    notes = models.TextField( blank=True )
+    
+    date_applied = models.DateField(null=True, blank=True)
+    deadline = models.DateField( null=True, blank=True)
+    interview_date = models.DateField( null=True, blank=True)
+    start_date = models.DateField( null = True , blank = True )
+    end_date = models.DateField( null = True , blank = True )
+
+
+
+
