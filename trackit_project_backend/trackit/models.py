0a1770dee2496f5c45a072093c31246b92727829
renad <xirnzsixteen@hotmail.com>
2025-04-30 10:11:35 +0300

    created Tracker model

--- trackit_project_backend/trackit/models.py
+++ trackit_project_backend/trackit/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 # Create your models here.
 
 
 class Tracker(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)    
     date_created = models.DateTimeField(auto_now_add=True)
 
