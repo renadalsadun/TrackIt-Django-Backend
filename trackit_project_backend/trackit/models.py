4654d597272baad66ddb010f9514e7df1e69ca99
renad <xirnzsixteen@hotmail.com>
2025-05-01 03:36:13 +0300

    created Document  model

--- trackit_project_backend/trackit/models.py
+++ trackit_project_backend/trackit/models.py
@@ -45,10 +45,17 @@ class Application(models.Model):
     interview_date = models.DateField( null=True, blank=True)
     start_date = models.DateField( null = True , blank = True )
     end_date = models.DateField( null = True , blank = True )
-    
+
     def __str__(self):
         return self.name
 
 
 
+class Document(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
+    name = models.CharField(max_length=100)
+    document_url = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
